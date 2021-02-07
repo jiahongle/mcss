95101aafbbba6213e47385756e95832be0401705
Alex <alex@chainsafe.io>
2021-02-07 10:44:25 -0500

    pushing code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
+import mcss from './mcss.jpg';
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={mcss} className="App-logo" alt="logo" />
+        <Header></Header>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ front-end/src/Header.js
@@ -0,0 +1,10 @@
+
+function Header() {
+    return (
+        <div className="Header">
+            hello
+        </div>
+    );
+}
+
+export default Header;
