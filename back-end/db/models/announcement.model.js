eba527c51bc04bc03bb7b88a4a77979e360197e2
Hamza Khan <hz.khan@mail.utoronto.ca>
2021-02-11 20:55:40 -0500

    [SUP-26] Modified mongoose schema for announcement - Hamza Khan, khanha11, hz.khan@mail.utoronto.ca, Jia Hong Le, lejia, jiahong.le@mail.utoronto.ca

--- back-end/db/models/announcement.model.js
+++ back-end/db/models/announcement.model.js
@@ -2,8 +2,12 @@
 'use strict';
 const mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
+
+// Might add custom id, depends on frontend implementation
+
 // Created Announcement schema: check with group if this is how they want it
-const AnnouncementSchema = new mongoose.Schema({
+const AnnouncementSchema = new Schema({
 	title: {
 		type: String,
 		required: true,
