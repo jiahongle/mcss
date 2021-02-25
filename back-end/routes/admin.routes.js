7cc298dafc030d7c2ca59ff3d0caecd24c72c24d
Hamza Khan <hz.khan@mail.utoronto.ca>
2021-02-24 22:48:27 -0500

    [SUP-40] Added the POST request for admin logging in - Hamza Khan, khanha11, hz.khan@mail.utoronto.ca, Jia Hong Le, lejia, jiahong.le@mail.utoronto.ca

--- back-end/routes/admin.routes.js
+++ back-end/routes/admin.routes.js
@@ -1,7 +1,6 @@
 // Model and express imports
 const router = require('express').Router();
 const Admin = require('../db/models/admin.model');
-
 // Set up a POST route to create a admin user
 router.post("/register", (req, res) => {
 
@@ -21,6 +20,26 @@ router.post("/register", (req, res) => {
         }
     );
 
+});
+
+// A route to login and create a session
+router.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Use the static method on the Patient model to find a patient
+    // by their email and password
+    Admin.findByUserNamePassword(username, password)
+        .then(admin => {
+            // Add the admin's id to the session cookie.
+            // We can check later if this exists to ensure we are logged in.
+            req.session.admin = admin._id;
+            req.session.username = admin.username;
+            res.send({ currentUser: admin.username });
+        })
+        .catch(error => {
+            error  === 'Username Not Found' ? res.status(400).send() : res.status(406).send() 
+        });
 });
 
 module.exports = router;
