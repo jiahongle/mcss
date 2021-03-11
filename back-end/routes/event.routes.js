03fa84730ee4ae049253c93a1ff5e90e75e72fe1
Hamza Khan <hz.khan@mail.utoronto.ca>
2021-03-10 21:29:32 -0500

    [SUP-64] Implements image uploading, sub event uploading and subevent patch - Hamza Khan, 1000569397, khanha11, Jia Hong, 1004886279, lejia

--- back-end/routes/event.routes.js
+++ back-end/routes/event.routes.js
@@ -25,7 +25,8 @@ router.get("/get", async (req, res) => {
 // The POST route: Check with front end team for any other additions/changes
 router.post("/post", async (req, res) => {
     try {
-        const { title, description, imgs, creator, signup } = req.body;
+        const { title, description, imgs, signup } = req.body;
+
         if (!title) {
             return res.status(400).json({ msg: "Title cannot be blank." });
         }
@@ -66,8 +67,8 @@ router.post("/post", async (req, res) => {
             title: title,
             description: description,
             imgs: img_array,
-            creator: creator,
-            signup : signup
+            signup : signup,
+            
         });
 
         const savedEvent = await event.save();
@@ -92,12 +93,12 @@ router.delete("/delete/:id", async (req, res) => {
         // console.log(savedImage);
 
         if (!savedEvent) {
-            res.status(404).json({ error: "Resource not found" });
+            return res.status(404).json({ error: "Resource not found" });
         }
-        else {
-            savedEvent.remove();
-            res.status(200).json({ msg: "Success" });
-        }
+        
+        savedEvent.remove();
+        res.status(200).json({ msg: "Success" });
+
     }
     catch (err) {
         res.status(500).json({ error: err.message });
@@ -112,27 +113,157 @@ router.patch("/update/:id", async (req, res) => {
         const retrievedEvent = await Event.findById(id);
 
         if (!retrievedEvent) {
-            res.status(404).json({ error: "Resource not found" });
+            return res.status(404).json({ error: "Resource not found" });
         }
-        else {
-            const { title, description, image, creator } = req.body;
-            if (!title) {
-                return res.status(400).json({ msg: "Title and body cannot be blank." });
-            }
 
-            retrievedEvent.title = title;
-            retrievedEvent.description = description;
-            retrievedEvent.img = image;
-            retrievedEvent.creator = creator;
+        const { title, description, signup} = req.body;
 
-            const savedEvent = await retrievedEvent.save();
-            res.send(savedEvent);
+        if (!title) {
+            return res.status(400).json({ msg: "Title cannot be blank." });
         }
+
+        retrievedEvent.title = title;
+        retrievedEvent.description = description;
+        retrievedEvent.signup = signup;
+
+        const savedEvent = await retrievedEvent.save();
+        res.send(savedEvent);
+        
     }
     catch (err) {
         res.status(500).json({ error: err.message });
     }
 })
 
+
+// Image routes
+
+router.post("/upload/:id", async (req, res) => {
+    try {
+        const {imgs} = req.body;
+
+        if (!imgs) {
+            return res.status(400).json({ msg: "No images provided." });
+        }
+
+        const id = req.params.id;
+
+        const retrievedEvent = await Event.findById(id);
+
+        if (!retrievedEvent) {
+            return res.status(404).json({ error: "Resource not found" });
+        }
+
+        var img_array = []
+
+        for (const img of imgs) {
+
+            var options = {
+                'method': 'POST',
+                'url': 'https://api.imgur.com/3/image',
+                'headers': {
+                    'Authorization': 'Client-ID 23cded91461ac64'
+                },
+                formData: {
+                    'image': img
+                }
+            };
+
+            const response = await requestPromise(options);
+
+            const jsonBody = JSON.parse(response.body);
+
+            const deletehash = jsonBody.data.deletehash;
+            const link = jsonBody.data.link;
+
+            const image = {
+                deletehash,
+                link
+            }
+
+            img_array.push(image);
+               
+        };
+
+        retrievedEvent.imgs = retrievedEvent.imgs.concat(img_array);
+
+        const savedEvent = await retrievedEvent.save();
+        res.send(savedEvent);
+
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+
+// Sub Event Routes
+
+router.post("/addsubevent/:id", async (req, res) => {
+    try {
+        const {title, description, signup} = req.body;
+
+        if (!title) {
+            return res.status(400).json({ msg: "No title provided." });
+        }
+
+        const id = req.params.id;
+
+        const retrievedEvent = await Event.findById(id);
+
+        if (!retrievedEvent) {
+            return res.status(404).json({ error: "Resource not found" });
+        }
+
+        const subevent = {
+            title,
+            description,
+            signup
+        }
+        
+        retrievedEvent.subevents.push(subevent)
+
+        const savedEvent = await retrievedEvent.save();
+        res.send(savedEvent);
+
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+router.patch("/updatesubevent/:id/:idx", async (req, res) => {
+    try {
+        const {title, description, signup} = req.body;
+
+        if (!title) {
+            return res.status(400).json({ msg: "No title provided." });
+        }
+
+        const id = req.params.id;
+        const index = req.params.idx;
+
+        const retrievedEvent = await Event.findById(id);
+
+        if (!retrievedEvent) {
+            return res.status(404).json({ error: "Resource not found" });
+        }
+
+        const subevent = {
+            title,
+            description,
+            signup
+        }
+        
+        retrievedEvent.subevents[index] = subevent;
+
+        const savedEvent = await retrievedEvent.save();
+        res.send(savedEvent);
+
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
 
 module.exports = router;
