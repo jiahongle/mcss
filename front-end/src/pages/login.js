f0f95c1cdacc58ed99945f496e6ba3c8136c3102
BCYilex <baichen.yan@mail.utoronto.ca>
2021-03-25 20:45:55 -0400

    Fixed the navbar logo too big on vertical monitor issue and enhanced UI for login page. -Baichen Yan, yanbaich

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import './login.css';
-import CreateEvent from '../components/events/createEvent.js'
-import DeleteEvent from '../components/events/deleteEvent.js'
-import EditEvent from '../components/events/editEvent.js'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { withRouter } from "react-router";
 
-export default class Login extends React.Component {
+
+class Login extends React.Component {
+    usernameField = React.createRef()
+    passwordField = React.createRef()
+
     state = {
         title: '',
         body: '',
         success: false,
+        attemptFailed: false
     }
 
-    onLogout = e => {
-        document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        localStorage.removeItem('jwt')
-        sessionStorage.removeItem('jwt')
-    }
-    onSubmit = e => {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: this.state.title, password: this.state.body }),
-            mode: 'cors',
-            credentials: 'include'
-        };
-        fetch('http://localhost:5000/admins/login', requestOptions).then(response => {
-            if (response.status === 200) {
+    // onLogout = e => {
+    //     document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    //     localStorage.removeItem('jwt')
+    //     sessionStorage.removeItem('jwt')
+    // }
 
-                this.setState({ success: true, title: '', body: '' })
-            }
-        });
+    onSubmit = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (this.state.title && this.state.body) {
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: this.state.title, password: this.state.body }),
+                mode: 'cors',
+                credentials: 'include'
+            };
+            fetch('http://localhost:5000/admins/login', requestOptions).then(response => {
+                if (response.status === 200) {
+                    this.setState({ success: true, title: '', body: '', attemptFailed: false })
+                    this.props.history.push('/')
+                } else {
+                    this.setState({attemptFailed: true})
+                }
+            });
+        } else {
+            this.setState({attemptFailed: true})
+        }
     }
     componentDidMount() {
         const requestOptions = {
@@ -48,55 +62,71 @@ export default class Login extends React.Component {
         });
     }
 
+    goDown = (e) => {
+        if (e.keyCode == 40) {
+            this.passwordField.current.focus()
+        }
+    }
+
+    goUp = (e) => {
+        if (e.keyCode == 38) {
+            this.usernameField.current.focus()
+        }
+    }
+
     render() {
         const { state } = this;
+        
 
         return (
             <div className="login-page">
-                <div className="announcement-container" >
-                    <div className="title-container">
-                        <b>Login</b>
-
+                {this.state.success? 
+                <div>
+                    <div>
+                        <b className="login-title">
+                            Already Logged In
+                        </b>
                     </div>
-                    {!this.state.success && <div className="announcement-post">
-
-                        <div>
-                            <div>
-                                <label className="announcement-form">
-                                    <p className="announcement-p">Username: </p>
-                                    <input
-                                        type="text"
-                                        value={state.title}
-                                        onChange={e => this.setState({ title: e.target.value })}
-                                        className="announcement-input" />
-                                </label>
+                </div>
+                :
+                <div>
+                    <b className="login-title">
+                        Login
+                    </b>
+                    <div className="login-box">
+                        <Form onSubmit={this.onSubmit}>
+                            <Form.Group size="lg" >
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control
+                                    autoFocus
+                                    ref={this.usernameField}
+                                    value={state.title}
+                                    onKeyUp={this.goDown}
+                                    onChange={(e) => this.setState({ title: e.target.value, attemptFailed: false })}
+                                />
+                                </Form.Group>
+                            <Form.Group size="lg">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    ref={this.passwordField}
+                                    type="password"
+                                    value={state.body}
+                                    onKeyUp={this.goUp}
+                                    onChange={(e) => this.setState({ body: e.target.value, attemptFailed: false })}
+                                />
+                                </Form.Group>
+                            <div className="login-failed">
+                                {this.state.attemptFailed && "Invalid username or password"}
                             </div>
-                            <div>
-                                <label className="announcement-form"> <p className="announcement-p">Password</p>
-                                    <input
-                                        type="password"
-                                        value={state.body}
-                                        onChange={e => this.setState({ body: e.target.value })}
-                                        className="announcement-input" />
-                                </label>
-                                <input type="submit" value="Submit" onClick={() => this.onSubmit()} />
-                            </div>
-                        </div>
-
-
-                    </div >}
-                    {
-                        this.state.success && <div> <div className="post-text"> Success! </div>
-                            <CreateEvent />
-                            <DeleteEvent />
-                            <EditEvent />
-                            <input type="submit" value="Logout" onClick={() => this.onLogout()} />
-
-                        </div >
-
-                    }
-                </div >
+                            <Button block size="lg" type="submit">
+                                Login
+                            </Button>
+                        </Form>
+                    </div >
+                </div>}
             </div>
         )
     }
 }
+
+export default withRouter(Login)
